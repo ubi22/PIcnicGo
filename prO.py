5625f4780ff9ad98f0a5dc161c2ff6ad4f31fd38
ubi22 <zoom.main21@gmail.com>
2022-10-04 17:55:27 +0300

    1.4.2

--- prO.py
+++ prO.py
@@ -7,19 +7,21 @@ class Test(MDApp):
 
     def build(self):
         self.theme_cls.material_style = "M3"
-
+        self.theme_cls.theme_style = "Dark"
         return Builder.load_string(
             '''
 MDScreen:
 
     MDBottomNavigation:
-        panel_color: 1, 1, 1, 0
+        #panel_color: "#eeeaea"
+        selected_color_background: "orange"
+        text_color_active: "lightgrey"
 
         MDBottomNavigationItem:
             name: 'screen 1'
             text: 'Mail'
-            icon: 'calculator-variant'
-            
+            icon: 'gmail'
+            badge_icon: "numeric-10"
 
             MDLabel:
                 text: 'Mail'
@@ -28,22 +30,21 @@ MDScreen:
         MDBottomNavigationItem:
             name: 'screen 2'
             text: 'Twitter'
-            icon: 'home'
-           
+            icon: 'twitter'
+            badge_icon: "numeric-5"
 
             MDLabel:
                 text: 'Twitter'
                 halign: 'center'
 
-
         MDBottomNavigationItem:
             name: 'screen 3'
             text: 'LinkedIN'
-            icon: '/PicniGo_1.4/Иконки/45200.png'
-            MDLabel
+            icon: 'linkedin'
 
-
-
+            MDLabel:
+                text: 'LinkedIN'
+                halign: 'center'
 '''
         )
 
